b929e28b975d4a447eecd0e20f5c7619720e7466
no-one2k <no-one2k@yandex.ru>
2025-04-03 09:07:33 +0000

    insignificant changes

--- prefect/tst.py
+++ prefect/tst.py
@@ -1,14 +1,32 @@
 from nba_api.stats.endpoints import LeagueGameFinder
 from nba_api.stats.library.parameters import LeagueIDNullable
 
+
+
+
 if __name__ == "__main__":
     start_date = '03/19/2025'
     end_date = '03/19/2025'
+    proxy_str = """host
+port
+username
+password
+    """
+    proxy_parts = [part.strip() for part in proxy_str.split('\n') if part.strip()]
+    host = proxy_parts[0]
+    port = proxy_parts[1]
+    username = proxy_parts[2]
+    password = proxy_parts[3]
+
+    proxy_auth = f"{username}:{password}@{host}:{port}"
+    proxy = [f"http://{proxy_auth}"]
+    print(proxy)
     game_finder = LeagueGameFinder(
-            league_id_nullable=LeagueIDNullable.nba,
-            date_from_nullable=start_date,
-            date_to_nullable=end_date,
-        )
+        league_id_nullable=LeagueIDNullable.nba,
+        date_from_nullable=start_date,
+        date_to_nullable=end_date,
+        proxy=proxy,
+    )
     print(game_finder)
     res = game_finder.get_normalized_dict()
     if res:
